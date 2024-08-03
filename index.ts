3d8605ccc1c6cea43f1ab83cf6bebcf40ee7c768
rozoomcool <rozoom.um@gmail.com>
2024-08-03 07:13:05 +0300

    cors setup

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ const io = new Server(server, {
   });
 
 const corsOptions = {
-    origin: 'http://localhost:3000/',
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Authorization"],
     credentials: true
